e491cf4322599b7f0e538d94c47353a985c8203d
unknown <japagraham@yahoo.com>
2019-04-08 07:21:43 -0400

    Stroe process file

---
+++ src/process.py
@@ -0,0 +1,57 @@
+import os
+import eo, e_math, time, im, utils
+
+def phase_0():
+
+    seed_file = [f for f in os.listdir('.') if f.startswith('seed_')][0] 
+    im.create('Takeoffs.fdf', seed_file)
+
+def phase_1(b):
+
+    b.create_zones()
+    b.rotate_space_polygons(270)
+
+def phase_2(b):
+
+    b.combine_close_vertices_within_floor(tol=0.75)
+
+    added = True
+    c = 0
+    while added:
+        c += 1
+        added = b.split_interior_walls(tol=0.75)
+
+def phase_3(b):
+
+    b.make_walls(short_iwall_names=True)
+    b.create_roofs()
+    b.create_ceilings()
+    b.create_floors()
+
+def phase_4(b):
+
+    for name in b.kinds('WINDOW'):
+        b.objects[name].delete()
+
+    b.make_windows('e_new.svg')
+    
+    
+def main():
+
+    phase_0()
+
+    input_file = utils.input_file_name()
+
+
+    b = eo.Building()
+    b.load(input_file)
+
+    phase_1(b)
+    phase_2(b)
+    phase_3(b)
+    phase_4(b)
+    b.dump()
+
+if __name__ == '__main__':
+    main()    
+
