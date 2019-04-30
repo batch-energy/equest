a7e16413c856b45914bfcc0c344708ee16d5ae83
unknown <japagraham@yahoo.com>
2019-04-29 20:38:34 -0400

    Print steps in default process.py

--- src/process.py
+++ src/process.py
@@ -3,18 +3,23 @@ import eo, e_math, time, im, utils
 
 def phase_0(pdf_file):
 
+    print '  Importing from PDF'
     seed_file = [f for f in os.listdir('.') if f.startswith('seed_')][0] 
     im.create(pdf_file, seed_file)
 
 def phase_1(b):
 
+    print '  Creating Zones'
     b.create_zones()
-    b.rotate_space_polygons(270)
+    print '  Rotating Spaces'
+    b.rotate_space_polygons(90)
 
 def phase_2(b):
 
+    print '  Combining Close Verteces'
     b.combine_close_vertices_within_floor(tol=0.5)
 
+    print '  Splitting Interior Polygon Sides'
     added = True
     c = 0
     while added:
@@ -23,21 +28,25 @@ def phase_2(b):
 
 def phase_3(b):
 
+    print '  Making Walls'
     b.make_walls(short_iwall_names=True)
+    print '  Making Roofs'
     b.create_roofs()
+    print '  Making Ceilings'
     b.create_ceilings()
+    print '  Making Floors'
     b.create_floors()
 
 def phase_4(b):
 
-    for name in b.kinds('WINDOW'):
+    print '  Importing Windows'
+    for name in b.kinds(['WINDOW', 'DOORS']):
         b.objects[name].delete()
-
     b.make_windows('e_new.svg')
-    
     
 def main():
 
+    print
     phase_0('Takeoffs.pdf')
 
     input_file = utils.input_file_name()
@@ -51,6 +60,8 @@ def main():
     phase_3(b)
     phase_4(b)
     b.dump()
+
+    print
 
 if __name__ == '__main__':
     main()    
